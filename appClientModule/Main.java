7f81349e784597542ca5dcf48717f7648cc75e46
Bruno Felipe De Matos <brunofelipe@rotadasbandeiras.com.br>
2017-11-02 18:09:45 -0200

    Ok... i'm trying

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,5 +1,6 @@
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -7,43 +8,86 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 	
 	private static Robot robot = null;
+	private static int locateTable = 1;
+	private static int alertLocateX = 0;
+	private static int alertLocateY = 0;
+	
 	
 	public static void main(String[] args) {
+		interactionWithUser();
+		threadChangeTab();
+	}	
+	
+	private static void interactionWithUser() {
+		Scanner scan = new Scanner (System.in);  
+		System.out.println("Where is the position of Alert?");
+		System.out.println("X is ...");
+		alertLocateX = scan.nextInt();
+		System.out.println("Y is ...");
+		alertLocateY = scan.nextInt();
+	}
+	
+	private static void threadChangeTab() {
 		try {
 			robot = new Robot();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			//THREAD JAVA 8
 			//Exemplo of Thread with Time for Schedule Executors...
 			ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
 			Runnable task = () -> {
 			    try {
 			        TimeUnit.SECONDS.sleep(2);
-			        //bclick(100, 100);
-			        //bclick(970,300);
-			        bclick(1210,300);
+			        if (locateTable == 1) {
+			        	changeTab(100,12);
+			        	confirmAlert(alertLocateX, alertLocateY);
+					}else if (locateTable == 2) {
+						changeTab(300,12);
+						confirmAlert(alertLocateX, alertLocateY);
+					}else if( locateTable == 3){
+						changeTab(500,12);
+						confirmAlert(alertLocateX, alertLocateY);
+					}else {
+						System.out.println("Do Nothing.");
+					}
 			    }
 			    catch (InterruptedException e) {
 			        System.err.println("task interrupted");
 			    }
 			};
-			executor.scheduleWithFixedDelay(task, 0, 60, TimeUnit.SECONDS);
+			executor.scheduleWithFixedDelay(task, 0, 30, TimeUnit.SECONDS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
+	
+	private static void confirmAlert(int x, int y) {
+		mouseMove(x, y);
+		System.out.println("Confirm");
+	}
+	
+	private static void changeTab(int x, int y) {
+		mouseMove(x, y);
+		System.out.println("Change Table!!!");
 
-	public Main() {
-		super();
 	}
 	
-	
-	public static void bclick (int x, int y) {
+	private static void mouseMove(int x, int y) {
 		robot.mouseMove(x, y);
+		clickAction();
+		if (locateTable == 1) {
+			locateTable++;
+		}else if (locateTable == 2) {
+			locateTable++;
+		} else {
+			locateTable = 1;
+		}
+	}
+	
+	private static void clickAction() {
 		robot.delay(2);
 		robot.mousePress(MouseEvent.BUTTON1_MASK);
 		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
-		System.out.println("Clicked!!!");
-
 	}
+	
+	
 
 }
